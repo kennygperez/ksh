31528fd1639b3bf4af044b7b59ae93c3fb3f9777
kennygperez <34965892+kennygperez@users.noreply.github.com>
2023-10-05 02:27:02 -0400

    Added go lang support

--- ksh.zsh
+++ ksh.zsh
@@ -9,12 +9,14 @@ kshell() {
 
 	KSH_CWD_CONTEXT=""
 
+	# GIT
 	if [ -d ".git" ]; then
 		KSH_GIT_TOKEN="%B%F{red}git%f%b: $(git branch --show-current)"
 
 		KSH_CWD_CONTEXT="$KSH_CWD_CONTEXT $KSH_GIT_TOKEN"
 	fi
 
+	# Node Package JSON
 	if [ -f "package.json" ]; then
 		KSH_NPM_TOKEN="%B%F{red}npm%f%b: $(npm --version)"
 		KSH_NODE_TOKEN="%B%F{green}node%f%b: $(node --version)"
@@ -22,10 +24,18 @@ kshell() {
 		KSH_CWD_CONTEXT="$KSH_CWD_CONTEXT $KSH_NPM_TOKEN $KSH_NODE_TOKEN"
 	fi
 
+	# Yarn
 	if [ -f "yarn.lock" ]; then
 		KSH_YARN_TOKEN="%B%F{blue}yarn%f%b: $(yarn --version)"
 
 		KSH_CWD_CONTEXT="$KSH_CWD_CONTEXT $KSH_YARN_TOKEN"
+	fi
+
+	# GO
+	if [ -f "go.mod" ]; then
+		KSH_GO_TOKEN="%B%F{blue}go%f%b: $(go version)"
+
+		KSH_CWD_CONTEXT="$KSH_CWD_CONTEXT $KSH_GO_TOKEN"
 	fi
 
 	if [ ! -z "$KSH_CWD_CONTEXT" ]; then
